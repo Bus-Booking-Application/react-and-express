ddf94beb23912258f7c8f0abc625a5af31db85f7
skdinesh <skdinesh97@gmail.com>
2025-01-25 11:18:43 +0530

    registe

--- express/src/user/register_model.js
+++ express/src/user/register_model.js
@@ -1,6 +1,11 @@
 const mongoose=require("mongoose")
+const {v4}=require("uuid")
 
-const user=new mongoose.model({
+const userSchema=new mongoose.Schema({
+    _id:{
+        type:String,
+        default: v4
+       },
     name:{
         type: String,
         require:True
@@ -9,10 +14,25 @@ const user=new mongoose.model({
         type:String,
         require:True
     },
-    sex:{
-        type:String
+    password:{
+        type:String,
     },
     phone:{
         type:Number
-    }
-})
+    },
+    sex:{
+        type:String
+    },
+    age: {
+        type: Date,
+        min: 18
+    },
+    
+},
+{
+    timestamps:true
+});
+
+const User=mongoose.model("User",userSchema);
+module.exports=User;
+
