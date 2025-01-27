55ee4769bed9bcf77d4a37cf90f46869713d7bc5
Roshan12321 <roshanakthar123321@gmail.com>
2025-01-27 12:14:16 +0530

    file del

---
+++ express/src/travels/model.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+
+const travel_schema = new mongoose.Schema({
+    CompanyName: String,
+    from: String,
+    to: String,
+    sector: {
+        type: String,
+        enum: ["government", "private"],
+        default: private
+    }
+})
