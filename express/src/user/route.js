47bd1353d1644acfb066ddc7ab4d489d19b9ccd2
Thenmozhi8610 <thenmozhi8610@gmail.com>
2025-01-29 15:16:25 +0530

    simple work

--- express/src/user/route.js
+++ express/src/user/route.js
@@ -2,19 +2,19 @@ const express=require("express")
 const router=express.Router()
 const controller=require("./controller")
 
-router
-.route("/post")
-.post(controller.postting)
+// router
+// .route("/post")
+// .post(controller.postting)
 
-router
-.route("/get")
-.get(controller.getting)
+// router
+// .route("/get")
+// .get(controller.getting)
 
-router
-.route("/update")
-.put(controller.update)
+// router
+// .route("/update")
+// .put(controller.update)
 
-router
-.route("/delete")
-.delete(controller.deleting)
+// router
+// .route("/delete")
+// .delete(controller.deleting)
 
