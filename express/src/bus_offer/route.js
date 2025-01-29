72b3985d871ef8a1ee7ed3e2c9a15ac0ee57dc4b
Roshan12321 <roshanakthar123321@gmail.com>
2025-01-29 10:59:36 +0530

    file delete

--- express/src/bus_offer/route.js
+++
@@ -1,8 +0,0 @@
-const express=require("express")
-const router=express.Router()
-const controller=require("../bus_offer/controller")
-
-router
-.route("/bus_offer_registration")
-.post(controller.register)
-
